c6e2829174ba66ea2751f1bc96b3f983c03c55f9
Roger Matinez <rmartinezvhsa@hotmail.com>
2018-08-08 00:10:43 -0500

    Agregando elementos para Home

--- app/src/main/java/com/a2mjgroup/buscoenelsur/Home.java
+++ app/src/main/java/com/a2mjgroup/buscoenelsur/Home.java
@@ -1,4 +1,19 @@
 package com.a2mjgroup.buscoenelsur;
 
-public class Home{
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class Home extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener{
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Intent intent = null;
+        if(menuItem.getItemId()== R.id.nav_menu) {
+            return true;
+        }
+        return false;
+    }
 }
